b6e563755cf00f2cfa02d94f7fb4390b33c49ba7
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-04-18 23:03:09 +0300

    Added basic code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Java-labs
+### Як запустити:
+* За допомогою команди ___git clone \<url to this project\>___ завантажуємо проєкт;
+* Відкриваємо командний рядок;
+* Командою ___cd \<path to downloaded project\>___ заходимо в корінь програми;
+* Вводимо команди:
+    1. ___mvn compile___;
+    2. ___mvn exec:java -Dexec.mainClass=ua.lviv.iot.App___;
+* Отримуємо результат нашої програми.
---
+++ src/main/java/ua/lviv/iot/models/mechanical_loom_types/DrawLoom.java
@@ -0,0 +1,24 @@
+package ua.lviv.iot.models.mechanical_loom_types;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import ua.lviv.iot.models.loom.Country;
+import ua.lviv.iot.models.loom_types.MechanicalLoom;
+import ua.lviv.iot.models.loom_types.Shape;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class DrawLoom extends MechanicalLoom {
+
+    public DrawLoom(Country originCountry, double price, double powerInWatts, int widthOfTheFormedTissue,
+                    String materialOfTheProducedFabric, Shape construction) {
+        super(originCountry, price, powerInWatts, widthOfTheFormedTissue, materialOfTheProducedFabric, construction);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
