9a00e52a12bbab7d2f5c1baf44c7e70299620ad0
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-05-15 12:59:38 +0300

    Added dependency for mvn site command

--- src/main/java/ua/lviv/iot/loomshop/dao/LoomDAO.java
+++ src/main/java/ua/lviv/iot/loomshop/dao/LoomDAO.java
@@ -1,7 +1,6 @@
 package ua.lviv.iot.loomshop.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.lviv.iot.loomshop.models.loom.Loom;
 
@@ -13,10 +12,4 @@ public interface LoomDAO extends JpaRepository<Loom, Long> {
     Loom findLoomById(Long id);
 
     Optional<Loom> findLoomsById(Long id);
-
-//    @Query(
-//            value = "SELECT * FROM Loom loom",
-//            nativeQuery = true
-//    )
-//    Optional<Loom> findAllLooms();
 }
